c4238344f516c74494e87ad71c704a0a2fbd278b
Dayrent Tjiang <dtji0003@student.monash.edu>
2025-04-25 16:02:10 +1000

    get all projects with skill

--- src/actions/project.ts
+++ src/actions/project.ts
@@ -23,6 +23,7 @@ export const getAvailableSkills = async () => {
 //get all projects
 export const getAllProjects = async () => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   const completeProject = [];
   try {
     const { data: projects, error } = await supabase.from("projects").select(`
@@ -53,15 +54,42 @@ export const getAllProjects = async () => {
     }
     return completeProject;
 =======
+=======
+  const completeProject = [];
+>>>>>>> a903bbb (get all projects with skill)
   try {
     const { data: projects, error } = await supabase.from("projects").select(`
-        *,
-        project_creator:users(*)
-      `);
+            *,
+            project_creator:users(*)
+        `);
 
     if (error) throw error;
+<<<<<<< HEAD
     return projects;
 >>>>>>> 7ded68b (get all projects)
+=======
+
+    //get all projects with skills, map through the projects and get the skills for each project and insert them into the completeProject object
+    for (let i = 0; i < projects.length; i++) {
+      const { data: projectSkills, error } = await supabase
+        .from("project_skills")
+        .select(
+          `
+                *,
+                skill:skills(*)
+            `
+        )
+        .eq("project_id", projects[i].project_id);
+
+      if (error) throw error;
+
+      completeProject.push({
+        ...projects[i],
+        skills: projectSkills
+      });
+    }
+    return completeProject;
+>>>>>>> a903bbb (get all projects with skill)
   } catch (error) {
     console.error("Error fetching projects:", error);
     return error;
